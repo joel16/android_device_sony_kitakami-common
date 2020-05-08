bc61022b091df1f5813b2c044ebbc4253c4a1568
Joel <joel16@iastate.edu>
2020-05-08 12:27:47 -0400

    Remove ifdef BOARD_PROVIDES_LIBRIL

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,7 +1,5 @@
 # Copyright 2006 The Android Open Source Project
 
-ifneq ($(BOARD_PROVIDES_LIBRIL),true)
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -46,5 +44,3 @@ LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif # BOARD_PROVIDES_LIBRIL
