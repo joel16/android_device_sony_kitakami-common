1b02aa751990e63bfff3f7462dccb9d3459a87d0
TARKZiM <tom8476oo@gmail.com>
2020-08-03 11:39:57 +0800

    kitakami-common: Build camera wrapper

--- device-common.mk
+++ device-common.mk
@@ -104,8 +104,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.msm8994 \
     Snap
 
 # Charger
