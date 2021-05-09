27cacbe758fb6ee6c5469fc1f0cea880ea543592
TARKZiM <tom8476oo@gmail.com>
2021-05-09 19:17:15 +0800

    kitakami-common: Build SoundTrigger HAL 2.0

--- device-common.mk
+++ device-common.mk
@@ -61,15 +61,13 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
