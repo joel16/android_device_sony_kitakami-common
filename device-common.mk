ba45d1bf434e33e82dad6e698c404f3531a42e58
Joel16 <joel16dev@gmail.com>
2022-01-18 23:05:20 -0500

    satsuki: Switch to OSS thermanager

--- device-common.mk
+++ device-common.mk
@@ -270,11 +270,6 @@ PRODUCT_PACKAGES += \
 
 # Thermal management
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
-PRODUCT_PACKAGES += \
-    thermal.msm8994 \
     thermanager
 
 # TimeKeep
